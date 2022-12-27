59f585bbeede8fe032220460aaaca5fe67ca8ca6
Dragana <exciarra@gmail.com>
2022-12-26 19:27:14 -0600

    Added new deals to cart

--- src/webapp/HomePage.java
+++ src/webapp/HomePage.java
@@ -2,16 +2,6 @@ package webapp;
 
 public class HomePage {
 
-    // Setting up homepage
-
-    // nav menu
-
-    // more things for the homepage
-
-    // logo
-
-    // ad more things
-
-    // footer
-
+    // facebook
+    // Twitter
 }
